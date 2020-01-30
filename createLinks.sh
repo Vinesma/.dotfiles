dc7784ef24418ae922abd802c94b7afaf6cde053
Vinesma <otaviocos_13@hotmail.com>
2020-01-30 10:17:57 -0300

    ~qbittorrent +createLinks.sh

--- createLinks.sh
+++ createLinks.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "Creating links... may overwrite some files."
 
 #mpv
 ln -s ${BASEDIR}/mpv ~/.config/mpv
+echo "Established 'mpv' link."
 
 #Clementine
 ln -s ${BASEDIR}/Clementine ~/.config/Clementine
+echo "Established 'Clementine' link."
 
 #qBittorrent
 ln -s ${BASEDIR}/qBittorrent ~/.config/qBittorrent
+echo "Established 'qBittorrent' link."
 
 #Aliases
 ln -s ${BASEDIR}/.bash_aliases ~/.bash_aliases
+echo "Established '.bash_aliases' link."
+
