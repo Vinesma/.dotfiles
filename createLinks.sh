42eff7f78a8242b82d6956f81d03e0d033151565
Vinesma <otaviocos_13@hotmail.com>
2020-01-30 21:02:01 -0300

    Reworked createLinks.sh

--- createLinks.sh
+++ createLinks.sh
@@ -4,35 +4,29 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIGDIR="$HOME/.config"
 git pull
 clear
-echo
-echo "Creating links... may overwrite some files."
+echo "Creating links... may overwrite some files.";echo
 
-#mpv
+echo "MPV:";echo
 MPVDIR="$CONFIGDIR/mpv"
 ln -svf ${BASEDIR}/mpv/mpv.conf ${MPVDIR}/mpv.conf
 ln -svf ${BASEDIR}/mpv/input.conf ${MPVDIR}/input.conf
 ln -svf ${BASEDIR}/mpv/input.conf.save ${MPVDIR}/input.conf.save
 echo
-echo "Established 'mpv' link."
 
-#Clementine
+echo "CLEMENTINE:";echo
 CLEMENTINEDIR="$CONFIGDIR/Clementine"
 ln -svf ${BASEDIR}/Clementine/Clementine.conf ${CLEMENTINEDIR}/Clementine.conf
 echo
-echo "Established 'Clementine' link."
 
-#qBittorrent
+echo "QBITTORRENT:";echo
 QBITTORRENTDIR="$CONFIGDIR/qBittorrent"
 ln -svf ${BASEDIR}/qBittorrent ${QBITTORRENTDIR}
 echo
-echo "Established 'qBittorrent' link."
 
-#.bashrc
+echo ".BASHRC:";echo
 ln -svf ${BASEDIR}/.bashrc ~/.bashrc
 echo
-echo "Established '.bashrc' link."
 
-#Aliases
+echo "ALIASES:";echo
 ln -svf ${BASEDIR}/.bash_aliases ~/.bash_aliases
 echo
-echo "Established '.bash_aliases' link."
