a02cec5b5dd5893882c9a04cbccccff5e8368a47
Vinesma <otaviocos_13@hotmail.com>
2020-05-18 12:10:24 -0300

    Update to add a 'open in browser' option to youtube videos in the script

--- newsboat/browser.sh
+++ newsboat/browser.sh
@@ -4,12 +4,13 @@ if [ $(echo $1 | grep youtube) ]
 then
     echo
     echo "This appears to be a youtube link, what to do?"
-    echo -e "1. Watch it\n2. Download it\n3. Exit"
+    echo -e "1. Watch it\n2. Download it\n3. Open in browser\n4. Exit"
     read option
     case $option in
         1) mpv --fullscreen=no --profile=youtube720p $1;;
         2) youtube-dl -f 22/18 $1;;
-        3) echo;;
+        3) firefox $1;;
+        4) echo;;
     esac
 else
     firefox $1
