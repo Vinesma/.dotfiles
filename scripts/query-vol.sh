f0ec600a43536792f00e281f79a9ab0bd337e445
Vinesma <otaviocos_13@hotmail.com>
2020-06-21 16:59:32 -0300

    Fix volume returning empty if at 100%

--- scripts/query-vol.sh
+++ scripts/query-vol.sh
@@ -4,7 +4,7 @@ notify_send_dir="/usr/bin/notify-send"
 
 current_vol=$(mpc | grep volume | cut -d' ' -f2)
 
-message="VOL: $current_vol"
+[[ $current_vol ]] && message="VOL: $current_vol" || message="VOL: 100%"
 
 $notify_send_dir -t 800 "MPD" "$message"
 
