b6cc97945e6b263d9088aaa443ee1173a64794df
Vinesma <otaviocos_13@hotmail.com>
2020-06-22 22:31:02 -0300

    Make auto-playlists use random and repeat mode

--- scripts/queue-epic.sh
+++ scripts/queue-epic.sh
@@ -5,6 +5,8 @@ mpc_dir="/usr/bin/mpc"
 
 $mpc_dir crop
 $mpc_dir search '(genre contains "epic")' | $mpc_dir add
+$mpc_dir random on
+$mpc_dir repeat on
 
 $notify_send_dir "MPD" "Queue set to 'epic'"
 
