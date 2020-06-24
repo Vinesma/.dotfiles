e919d4791cef600e7f8eca15e958cd8183f252db
Vinesma <otaviocos_13@hotmail.com>
2020-06-24 16:37:18 -0300

    Add failure notification when ping returns no message

--- scripts/conc-notify.sh
+++ scripts/conc-notify.sh
@@ -3,8 +3,8 @@
 notify_send_dir="/usr/bin/notify-send"
 
 state=$(nmcli n connectivity check)
-packet_status=$(ping -c 5 google.com | grep "packet loss" | cut -d',' -f3)
+packet_status=$(ping -c 10 google.com | grep "packet loss" | cut -d',' -f3)
 
-message="STATE: $state\nPACKETS:$packet_status"
+[[ $packet_status ]] && message="STATE: $state\nPACKETS:$packet_status" || message="STATE: $state\nPING FAILED"
 
 $notify_send_dir "CONNECTION STATUS" "$message"
