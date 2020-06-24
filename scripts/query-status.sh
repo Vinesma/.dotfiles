c2b9acb4620c3208fe657f932e106e0761aaa837
Vinesma <otaviocos_13@hotmail.com>
2020-06-24 20:35:58 -0300

    Add script that sends music notifications whenever steam is running
    - For when I'm playing a game or something

--- scripts/query-status.sh
+++ scripts/query-status.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# Queries music status if steam is running in the background (presumably a game is playing in fullscreen)
 
 notify_send_dir="/usr/bin/notify-send"
+pgrep_dir="/usr/bin/pgrep"
 
 current_status=$(mpc)
 
-message="$current_status"
+title=$(echo "$current_status" | head -n 1)
+message=$(echo "$current_status" | tail -n +2)
 
-$notify_send_dir "MPD" "$message"
+$pgrep_dir steam > /dev/null && $notify_send_dir "$title" "$message"
 
 
 
