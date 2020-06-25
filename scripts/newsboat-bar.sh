c568b8cab9bb46de177f3c0975f445f4dfb3ab98
Vinesma <otaviocos_13@hotmail.com>
2020-06-25 18:28:26 -0300

    Modify newsboat-bar script to stop reloading newsboat

--- scripts/newsboat-bar.sh
+++ scripts/newsboat-bar.sh
@@ -5,10 +5,10 @@ newsboat_path="/usr/bin/newsboat"
 # This script can be used to reload newsboat but that means the bar can't be updated as often
 # Just comment out the next 3 lines if you don't want to reload
 
-export DISPLAY=:0
-export DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1000/bus'
-newsboat -x reload
+# export DISPLAY=:0
+# export DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1000/bus'
+# newsboat -x reload
 
 unread_articles=$(newsboat -x print-unread | cut -d' ' -f 1)
 
-[ "$unread_articles" ] && echo "[RSS] #$unread_articles" || echo ""
+[ "$unread_articles" ] && echo "[RSS] #$unread_articles" || echo "[RSS]"
