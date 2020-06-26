b2da95e1b71cc65ef043ca4ede4132cfa64fce28
Vinesma <otaviocos_13@hotmail.com>
2020-06-26 20:37:08 -0300

    Add options to view youtube thumbnails and length inside newsboat

--- newsboat/browser.sh
+++ newsboat/browser.sh
@@ -4,13 +4,15 @@ if [ $(echo $1 | grep youtube.com) ]
 then
     echo
     echo "This appears to be a youtube link, what to do?"
-    echo -e "1. Watch it\n2. Download it\n3. Open in browser\n4. Exit"
+    echo -e "1. Watch it\n2. Download it\n3. Open in browser\n4. Print video length\n5. Show video thumbnail\n6. Exit"
     read option
     case $option in
         1) mpv --fullscreen=no --profile=youtube720p $1;;
         2) youtube-dl -f 22/18 $1;;
         3) firefox $1;;
-        4) echo;;
+	4) notify-send "NEWSBOAT:" "Video length: $(youtube-dl --get-duration $1)";;
+	5) feh -F "$(youtube-dl --get-thumbnail $1)";;
+        *) echo;;
     esac
 else
     firefox $1
