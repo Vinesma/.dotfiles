4a6f4a97033ff3e934f585ee70281b66647a0e93
Vinesma <otaviocos_13@hotmail.com>
2020-06-27 15:18:18 -0300

    Add function that shows video thumbnail with video length in the image

--- newsboat/browser.sh
+++ newsboat/browser.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+video-info() {
+        local video=$(youtube-dl --get-title --get-duration --get-thumbnail $1)
+        local title=$(echo "$video" | head -n 1)
+        local thumbnail=$(echo "$video" | head -n 2 | tail -n 1)
+        local duration=$(echo "$video" | tail -n 1)
+        feh "$thumbnail" -F --title "$title" --info "echo \"LENGTH: $duration\""
+}
+
 if [ $(echo $1 | grep youtube.com) ]
 then
     echo
     echo "This appears to be a youtube link, what to do?"
-    echo -e "1. Watch it\n2. Download it\n3. Open in browser\n4. Print video length\n5. Show video thumbnail\n6. Exit"
+    echo -e "1. Watch it\n2. Download it\n3. Open in browser\n4. Show video info\n5. Exit"
     read option
     case $option in
         1) mpv --fullscreen=no --profile=youtube720p $1;;
         2) youtube-dl -f 22/18 $1;;
         3) firefox $1;;
-	4) notify-send "NEWSBOAT:" "Video length: $(youtube-dl --get-duration $1)";;
-	5) feh -F "$(youtube-dl --get-thumbnail $1)";;
+	4) video-info $1;;
         *) echo;;
     esac
 else
