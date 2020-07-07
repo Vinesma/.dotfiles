a85162028cbd4e0b3e141e0d0b646b1cd6d6bb36
Vinesma <otaviocos_13@hotmail.com>
2020-07-07 20:37:43 -0300

    Fix wrong cut field and format

--- scripts/curl-weather.sh
+++ scripts/curl-weather.sh
@@ -3,16 +3,16 @@
 curl_dir="/usr/bin/curl"
 site="wttr.in"
 city="Petrolina"
-format="+%C+%t+%p"
+format="+%t+%p+%C"
 
 response=$($curl_dir -s "$site"/"$city"?format="$format")
 
 assign_icon() {
-	local weather_info=$(echo "$response" | cut -d' ' -f 4-)
+	local weather_info=$(echo "$response" | cut -d' ' -f -3)
 	case "$response" in
-		*Partly\ cloudy*) echo "杖 $weather_info" ;;
-		*Light\ rain*) echo "殺 $weather_info" ;;
-		*Clear*|*Sunny*) echo "滛 $weather_info" ;;
+		*Partly\ cloudy*) echo "杖$weather_info" ;;
+		*Light\ rain*) echo "殺$weather_info" ;;
+		*Clear*|*Sunny*) echo "滛$weather_info" ;;
 		*) echo "$response" ;;
 	esac
 }
