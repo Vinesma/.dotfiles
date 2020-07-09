faf83191efb15970dc6a9bd69792677da9cedd70
Vinesma <otaviocos_13@hotmail.com>
2020-07-09 20:44:23 -0300

    Add new weather icons

--- scripts/curl-weather.sh
+++ scripts/curl-weather.sh
@@ -16,8 +16,9 @@ response=$($curl_dir -s "$site"/"$city"?format="$format")
 assign_icon() {
     local weather_info=$(echo "$response" | cut -d' ' -f -3)
     case "$response" in
-        *Partly\ cloudy*) echo "杖$weather_info" ;;
+        *Partly\ cloudy*|*Overcast*) echo "杖$weather_info" ;;
         *Light\ rain*) echo "殺$weather_info" ;;
+        *Cloudy*) echo "摒$weather_info" ;;
         *Clear*|*Sunny*) echo "滛$weather_info" ;;
         *) echo "$response" ;;
     esac
