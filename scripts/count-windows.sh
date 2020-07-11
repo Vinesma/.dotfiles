008d3ea5cb860804dc2e5d58757c20279297d76f
Vinesma <otaviocos_13@hotmail.com>
2020-07-11 18:46:01 -0300

    Add window counter to polybar
    - Shows a counter when (amount of open windows) > 1

---
+++ scripts/count-windows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Script meant to be run by a polybar module, returns number of open windows.
+#
+
+qtilecmd_dir="/usr/bin/qtile-cmd"
+
+window_count="$($qtilecmd_dir -o cmd -f windows | grep name | wc -l)"
+# no. of windows minus polybar
+true_count="$(( $window_count - 1 ))"
+
+[[ "$true_count" -gt 1 ]] && echo "$true_count" || echo ""
