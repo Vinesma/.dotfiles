365c2e4384cc2e986eaf1aecb9ab167bb9c94a0f
Vinesma <otaviocos_13@hotmail.com>
2020-07-12 13:02:06 -0300

    Add error handling and improve formatting in clipboard-mpv

--- scripts/clipboard-mpv.sh
+++ scripts/clipboard-mpv.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 #
 # Script meant to be run by a qtile keybind, opens a rofi menu for watching videos in mpv.
-#
+# Features:
+# - VIDEO: Choose video format and watch straight from the rofi menu after a single keypress
+# Dependencies:
+# mpv, xclip, youtube-dl, rofi
 
 mpv_dir="/usr/bin/mpv"
 xclip_dir="/usr/bin/xclip"
 youtubedl_dir="/usr/bin/youtube-dl"
 rofi_dir="/usr/bin/rofi"
-mpv_dir="/usr/bin/mpv"
 link="$($xclip_dir -o)"
 
-get-video-info() {
-    notify-send -t 1800 "MPV" "[youtube-dl] loading video information..."
-    video_info="$($youtubedl_dir -F $link)"
+send-error() {
+    notify-send -t 1800 "MPV" "$1"
+}
 
-    format=$(echo "$video_info" | \
+start-playback() {
+    notify-send -t 1800 "MPV" "Starting playback..."
+    "$mpv_dir" --ytdl-format="$1" "$link" &
+}
+
+parse-video-info() {
+    format=$(echo "$1" | \
     awk '{print $1 "  " $3}' | \
     sed '1,3d' | \
     sort | \
     "$rofi_dir" -dmenu | \
     cut -d' ' -f 1)
 
-    notify-send -t 1800 "MPV" "starting playback..."
-    "$mpv_dir" --ytdl-format="$format" "$link" &
+    start-playback "$format"
 }
 
-send-error() {
-    notify-send -t 1800 "MPV" "$1"
-    exit 1
+get-video-info() {
+    notify-send -t 1800 "MPV" "[youtube-dl] Loading video information..."
+    video_info="$($youtubedl_dir -F $link)"
+
+    [[ $? -eq 0 ]] && parse-video-info "$video_info" || \
+        send-error '[youtube-dl] Error while fetching video info' && exit 1
 }
 
 case "$link" in
     *youtube.com*|*youtu.be*|*twitch.tv*) get-video-info ;;
-    *) send-error "unsupported link, exiting" ;;
+    *) send-error "Unsupported link, exiting" && exit 1 ;;
 esac
