9e618132a39c4d20741c4d6c23c715a2c4031f36
Vinesma <otaviocos_13@hotmail.com>
2020-07-20 20:42:03 -0300

    Add redshift module

---
+++ scripts/tempsetter-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Simple script meant to be used as a module, shows up if redshift is running.
+# Named tempsetter check to avoid being murdered by any killall command.
+
+if pgrep -u "$UID" redshift >/dev/null; then
+    echo "ﯦ"
+else
+    echo "ﯧ"
+fi
