8a82fbfc2c1c4ad4dc13cbce99150671a490805a
Vinesma <otaviocos_13@hotmail.com>
2020-07-21 14:29:46 -0300

    Improve code in conc-notify

--- scripts/conc-notify.sh
+++ scripts/conc-notify.sh
@@ -3,8 +3,11 @@
 notify_send_dir="/usr/bin/notify-send"
 
 state=$(nmcli n connectivity check)
-packet_status=$(ping -c 10 google.com | grep "packet loss" | cut -d',' -f3)
 
-[[ $packet_status ]] && message="STATE: $state\nPACKETS:$packet_status" || message="STATE: $state\nPING FAILED"
+if packet_status=$(ping -c 10 google.com | grep "packet loss" | cut -d',' -f3); then
+    message="STATE: $state\nPACKETS:$packet_status"
+else
+    message="STATE: $state\nPING FAILED"
+fi
 
 $notify_send_dir "CONNECTION STATUS" "$message"
