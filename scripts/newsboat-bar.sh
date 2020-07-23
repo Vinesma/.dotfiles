f70b736b1a8da6da75b0a6eac92fb114bffa6e08
Vinesma <otaviocos_13@hotmail.com>
2020-07-23 11:44:52 -0300

    Improve code in newsboat-bar

--- scripts/newsboat-bar.sh
+++ scripts/newsboat-bar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This script can be used to reload newsboat but that means the bar can't be updated as often
 # Just comment out the next 3 lines if you don't want to reload
 
@@ -6,6 +6,12 @@
 # export DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1000/bus'
 # newsboat -x reload
 
-unread_articles=$(newsboat -x print-unread | cut -d' ' -f 1)
-
-[ "$unread_articles" ] && [ "$unread_articles" -gt 0 ] && echo "$unread_articles" || echo ""
+if unread_articles=$(newsboat -x print-unread | cut -d' ' -f 1) && [[ "$unread_articles" -gt 50 ]]; then
+    if [[ "$unread_articles" -gt 500 ]]; then
+        echo "500+"
+    else
+        echo "$unread_articles"
+    fi
+else
+    echo ""
+fi
