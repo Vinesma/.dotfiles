ae8c4b7c48634e0e8cff4c96a0189dbcfb61d51d
Vinesma <otaviocos_13@hotmail.com>
2020-08-13 19:06:59 -0300

    queue-clear is a bit smarter

--- scripts/queue-clear.sh
+++ scripts/queue-clear.sh
@@ -2,8 +2,13 @@
 
 icon="/usr/share/icons/Papirus/32x32/apps/mpd.svg"
 
-mpc crop
-mpc repeat off
+# IF a song is playing THEN don't interrupt : IF stopped THEN clear the whole queue.
+if [[ $(mpc | wc -l) -ne 3 ]]; then
+    mpc clear
+else
+    mpc crop
+    mpc repeat off
+fi
 
 notify-send -i "$icon" "MPD" "Queue cleared!"
 
