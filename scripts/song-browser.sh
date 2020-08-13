661abded80c963488667ae8b4f73307aa1a6fbae
Vinesma <otaviocos_13@hotmail.com>
2020-08-13 18:04:33 -0300

    Improve song-browser

--- scripts/song-browser.sh
+++ scripts/song-browser.sh
@@ -19,18 +19,29 @@ add-album() {
     notify-send -i "$icon" "MPD" "$1 added to queue!"
 }
 
+show-all-music() {
+    local track
+    track=$(mpc listall | rofi -dmenu -no-custom -i -p ' Play one' -lines 15)
+    echo "$track" | mpc add
+    mpc play
+}
+
 show-menu() {
     local all_albums
+    local album
     all_albums=$(ls "$music_folder")
     
-    album=$(echo -e " Exit\n Clear Queue\n$all_albums" | \
-    rofi -dmenu -no-custom -i -p ' Play' -lines 10)
+    while true; do
+        album=$(echo -e " Exit\n Clear Queue\n All Tracks\n$all_albums" | \
+        rofi -dmenu -no-custom -i -p ' Play' -lines 10)
 
-    case "$album" in
-        *Exit) exit ;;
-        *Clear\ Queue) clear-queue ;;
-        *) add-album "$album" ;;
-    esac
+        case "$album" in
+            *Exit) exit ;;
+            *Clear\ Queue) clear-queue ;;
+            *All\ Tracks) show-all-music ;;
+            *) add-album "$album" ;;
+        esac
+    done
 }
 
 show-menu
