3c9da69bfc36a1081a6c1e2bc0ebfa0bbf4f6fc9
Vinesma <otaviocos_13@hotmail.com>
2020-08-16 20:01:53 -0300

    Add syncing of newsboat feeds by using syncthing

--- scripts/newsboat/open-newsboat.sh
+++ scripts/newsboat/open-newsboat.sh
@@ -4,8 +4,13 @@
 # Dependencies
 # - newsboat
 
+[[ -e "$sync_folder/.newsboat_archive" ]] && newsboat -q -I "$sync_folder/.newsboat_archive"
+
 newsboat -q
 
 folder="$HOME/.dotfiles/scripts/newsboat"
+sync_folder="$HOME/Sync"
 
 newsboat -x print-unread | cut -d' ' -f 1 > "$folder/unread_count.tmp"
+
+[[ -e "$sync_folder" ]] && newsboat -q -E "$sync_folder/.newsboat_archive"
