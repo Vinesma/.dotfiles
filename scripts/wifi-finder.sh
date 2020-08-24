e086e8806579e238e76773e03b85c5e099961c13
Vinesma <otaviocos_13@hotmail.com>
2020-08-24 17:36:57 -0300

    Add fail conditions to wifi-finder

--- scripts/wifi-finder.sh
+++ scripts/wifi-finder.sh
@@ -4,14 +4,18 @@
 # - Dependencies:
 # nmcli, rofi
 
+icon_error="/usr/share/icons/Papirus/32x32/status/dialog-error.svg"
+icon_connected="/usr/share/icons/Papirus/32x32/status/online.svg"
+icon_failed="/usr/share/icons/Papirus/32x32/status/offline.svg"
+
 password-menu() {
     local pass
     pass=$(rofi -dmenu -password -p 'Password' -lines 0)
 
     if nmcli device wifi connect "$1" password "$pass"; then
-        notify-send "Wi-fi" "Now connected to '$1'"
+        notify-send -i "$icon_connected" "Wifi-finder" "Now connected to '$1'."
     else
-        notify-send "Wi-fi" "Connection failed..."
+        notify-send -i "$icon_failed" "Wifi-finder" "Connection failed..."
     fi
 }
 
@@ -19,10 +23,14 @@ show-menu() {
     local selection
     selection=$(nmcli -f BARS,RATE,SSID device wifi list | sed '1d')
 
-    selection=$(echo -e "$available" | rofi -dmenu -only-match -i -p 'Wifi list' -lines 10)
-    ssid=$(echo "$selection" | cut -d' ' -f 6- | sed 's/\s\+$//g')
+    if [[ -n "$selection" ]]; then
+        selection=$(echo -e "$available" | rofi -dmenu -only-match -i -p 'Wifi list' -lines 10)
+        ssid=$(echo "$selection" | cut -d' ' -f 6- | sed 's/\s\+$//g')
 
-    password-menu "$ssid"
+        password-menu "$ssid"
+    else
+        notify-send -i "$icon_error" "Wifi-finder" "Not near any wifi networks, or no wifi device found."
+    fi
 }
 
 show-menu
