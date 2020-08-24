d22d8781fb054e7ae506d628c5d6243b8a4abe72
Vinesma <otaviocos_13@hotmail.com>
2020-08-24 14:45:39 -0300

    wifi-finder feature complete, needs testing

--- scripts/wifi-finder.sh
+++ scripts/wifi-finder.sh
@@ -8,15 +8,19 @@ password-menu() {
     local pass
     pass=$(rofi -dmenu -password -p 'Password' -lines 0)
 
-    echo "$1 - $pass"
+    if nmcli device wifi connect "$1" password "$pass"; then
+        notify-send "Wi-fi" "Now connected to '$1'"
+    else
+        notify-send "Wi-fi" "Connection failed..."
+    fi
 }
 
 show-menu() {
-    local available
-    available=$(nmcli -f BARS,RATE,SSID device wifi list | sed '1d')
+    local selection
+    selection=$(nmcli -f BARS,RATE,SSID device wifi list | sed '1d')
 
     selection=$(echo -e "$available" | rofi -dmenu -only-match -i -p 'Wifi list' -lines 10)
-    ssid=$(echo "$selection" | cut -d' ' -f 6-)
+    ssid=$(echo "$selection" | cut -d' ' -f 6- | sed 's/\s\+$//g')
 
     password-menu "$ssid"
 }
