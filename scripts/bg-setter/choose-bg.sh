9cba71b4dc943edbe0cdbe25a767d9defaec5831
Vinesma <otaviocos_13@hotmail.com>
2020-08-25 10:51:09 -0300

    choose-bg is done

--- scripts/bg-setter/choose-bg.sh
+++ scripts/bg-setter/choose-bg.sh
@@ -2,13 +2,21 @@
 
 # Choose several options for wallpaper setting.
 # Dependencies:
-# feh, rofi
+# feh, rofi, magick
 
 wallpaper_dir="$HOME/Pictures/Wallpapers"
 files_folder="$HOME/.dotfiles/scripts/bg-setter"
 resolution=$(xrandr | grep '*' | awk '{printf $1}')
 width=$(echo "$resolution" | cut -d 'x' -f 1)
 height=$(echo "$resolution" | cut -d 'x' -f 2)
+
+notify_time=2000
+icon_error="/usr/share/icons/Papirus/32x32/status/dialog-error.svg"
+icon_download="/usr/share/icons/Papirus/32x32/emblems/emblem-downloads.svg"
+
+send-error() {
+    notify-send -i "$icon_error"  -t "$notify_time" "choose-bg" "$1"
+}
 
 load-config() {
     if [[ -e "$files_folder/saturation" ]]; then
@@ -42,25 +50,78 @@ clear-cache() {
     wal -c
 }
 
+resize-image() {
+    local accepted
+    local gravity
+    accepted=1
+
+    while [[ "$accepted" -eq 1 ]]; do
+        gravity=$(echo -e "NorthWest\nNorth\nNorthEast\nWest\nCenter\nEast\nSouthWest\nSouth\nSouthEast" \
+            | rofi -dmenu -only-match -p 'Center image at' -lines 9)
+
+        if magick "$files_folder/output$1" \
+            -resize "$width"x"$height"^ \
+            -gravity "$gravity" \
+            -extent "$width"x"$height" \
+            "$files_folder/output-final$1"; \
+        then
+            accepted=$(feh -G \
+                --action ';[Accept this image]echo 0' \
+                --action1 ';[Reject and try again]echo 1' \
+                --action2 ';[Exit]echo 3' \
+                -F \
+                "$files_folder/output-final$1")
+            accepted=$(echo "$accepted" | tail -n 1)
+        else
+            send-error "Download failed or image conversion failed..."
+            accepted=3
+        fi
+    done
+
+    if [[ "$accepted" -eq 0 ]]; then
+        filename="$wallpaper_dir/wallpaper-$RANDOM-$RANDOM-IMG$1"
+        mv -f "$files_folder/output-final$1" "$filename"
+        . "$files_folder/set-bg.sh" "$filename" --saturate "$saturation_amount" > "$files_folder/setbg-log"
+    fi
+
+    rm "$files_folder/output$1"
+    rm "$files_folder/output-final$1"
+}
+
+download-image() {
+    local link
+    link=$(xclip -o)
+
+    if extension=$(echo "$link" | grep -o '\(\.jpg\|\.jpeg\|\.png\)'); then
+        notify-send -i "$icon_download" "choose-bg" "Downloading image"
+        curl -s "$link" -o "$files_folder/output$extension"
+        resize-image "$extension"
+    else
+        send-error "Unsupported link."
+    fi
+}
+
 show-chooser() {
     local wallpaper
     wallpaper=$(feh -G -A ';[Choose this image]echo %F' --max-dimension "$width"x"$height" -d -F "$wallpaper_dir")
     wallpaper=$(echo "$wallpaper" | tail -n 1)
 
     if [[ -n "$wallpaper" ]]; then
-        . "$files_folder/set-bg.sh" "$wallpaper" --saturate "$saturation_amount"
+        . "$files_folder/set-bg.sh" "$wallpaper" --saturate "$saturation_amount" > "$files_folder/setbg-log"
     fi
 }
 
 show-menu() {
     local option
-    option=$(echo -e "1. Pick wallpapers\n2. Clear cache\n3. Set saturation\n4. Set transparency\n5. Exit" | rofi -dmenu -only-match -p 'option' -format 'd' -mesg "Saturation: $saturation_amount" -lines 5)
+    option=$(echo -e "1  Pick wallpapers\n2  Download image\n3  Clear cache\n4 Set saturation\n5 Set transparency\n6  Exit" \
+        | rofi -dmenu -only-match -p 'option' -format 'd' -mesg "Saturation: $saturation_amount" -lines 6)
 
     case "$option" in
         1) show-chooser ;;
-        2) clear-cache ;;
-        3) set-saturation ;;
-        4) set-transparency ;;
+        2) download-image ;;
+        3) clear-cache ;;
+        4) set-saturation ;;
+        5) set-transparency ;;
         *) exit ;;
     esac
 }
