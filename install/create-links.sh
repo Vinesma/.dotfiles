8c8e3f252f931fd11e9a4512476e51d398c76072
Vinesma <otaviocos_13@hotmail.com>
2020-11-18 22:27:34 -0300

    Streamline function

--- install/create-links.sh
+++ install/create-links.sh
@@ -3,75 +3,51 @@
 config_dir="$HOME/.config"
 dotfiles_dir="$HOME/.dotfiles"
 
-echo "==> Linking files, this makes sure any changes in the filesystem get reflected on the repository and can be committed or reverted."
-echo "-> [i] Files will be overwritten."
+# Load helper functions
+. "$main_folder/helper-functions.sh"
+
+arrow-msg "Linking files, this makes sure any changes in the filesystem get reflected on the repository and can be committed or reverted."
+info-msg "Files will be overwritten."
 
 # Used to create links between the repository and $HOME
 create-link-to-home() {
-    ln -sfv "$dotfiles_dir"/"$1" "$HOME"/"$1"
+    arrow-msg "$1"
+    ln -sfv "$dotfiles_dir"/"$2" "$HOME"/"$2"
     echo
 }
 
 # Used to create links between the repository and the config directory
 create-link-to-config() {
-    ln -sfv "$dotfiles_dir"/"$1" "$config_dir"/"$1"
+    arrow-msg "$1"
+    ln -sfv "$dotfiles_dir"/"$2" "$config_dir"/"$2"
     echo
 }
 
 # Used to create links between the repository and the config directory
 # Use when the paths are not exactly the same
 create-link-to-config-diff() {
-    ln -sfv "$dotfiles_dir"/"$1" "$config_dir"/"$2"
+    arrow-msg "$1"
+    ln -sfv "$dotfiles_dir"/"$2" "$config_dir"/"$3"
     echo
 }
 
-echo "- BASHRC";echo
-create-link-to-home ".bashrc"
+create-link-to-home "BASHRC" ".bashrc"
+create-link-to-home "ZSH" ".zshrc"
+create-link-to-config "NANO" "nano/nanorc"
+create-link-to-config "KITTY" "kitty/kitty.conf"
+create-link-to-config-diff "REDSHIFT" "redshift/redshift.conf" "redshift.conf"
+create-link-to-config "NEWSBOAT" "newsboat/urls"
+create-link-to-config "NEWSBOAT" "newsboat/config"
+create-link-to-config "MPV" "mpv/mpv.conf"
+create-link-to-config "MPV" "mpv/input.conf"
+create-link-to-config "YOUTUBE-DL" "youtube-dl/config"
+create-link-to-config "PICOM" "picom/picom.conf"
+create-link-to-config "MPD" "mpd/mpd.conf"
+create-link-to-config "NCMPCPP" "ncmpcpp/config"
+create-link-to-config "NCMPCPP" "ncmpcpp/bindings"
+create-link-to-config "ROFI" "rofi/config.rasi"
+create-link-to-config "QTILE" "qtile/config.py"
+create-link-to-config "NVIM" "nvim/init.vim"
+create-link-to-config "ZATHURA" "zathura/zathurarc"
 
-echo "- NANO";echo
-create-link-to-config "nano/nanorc"
-
-echo "- KITTY";echo
-create-link-to-config "kitty/kitty.conf"
-
-echo "- REDSHIFT";echo
-create-link-to-config-diff "redshift/redshift.conf" "redshift.conf"
-
-echo "- NEWSBOAT";echo
-create-link-to-config "newsboat/urls"
-create-link-to-config "newsboat/config"
-
-echo "- MPV";echo
-create-link-to-config "mpv/mpv.conf"
-create-link-to-config "mpv/input.conf"
-
-echo "- YOUTUBE-DL";echo
-create-link-to-config "youtube-dl/config"
-
-echo "- PICOM";echo
-create-link-to-config "picom/picom.conf"
-
-echo "- MPD";echo
-create-link-to-config "mpd/mpd.conf"
-
-echo "- NCMPCPP";echo
-create-link-to-config "ncmpcpp/config"
-create-link-to-config "ncmpcpp/bindings"
-
-echo "- ROFI";echo
-create-link-to-config "rofi/config.rasi"
-
-echo "- QTILE";echo
-create-link-to-config "qtile/config.py"
-
-echo "- NVIM";echo
-create-link-to-config "nvim/init.vim"
-
-echo "- ZATHURA";echo
-create-link-to-config "zathura/zathurarc"
-
-# TEMPLATE
-# echo "- {NAME}";echo
-# create-link-to-{foo} "{path}"
-
-echo "-> Done."
+arrow-msg "Done."
