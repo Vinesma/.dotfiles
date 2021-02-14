b3c3d1f39eb4f68c2d3169924963cba216e01740
Vinesma <otaviocos_13@hotmail.com>
2021-02-14 16:15:41 -0300

    Fix yay packages refusing to be installed as sudo

--- install/installers/aur.py
+++ install/installers/aur.py
@@ -10,13 +10,13 @@ def sync(args=[]):
     Sync repos and install packages
     """
     if len(args) > 0:
-        spawn.sudo_process(aur_helper, ["--noconfirm", "-Sua"] + args)
+        spawn.process(aur_helper, ["--noconfirm", "-Sua"] + args)
     else:
-        spawn.sudo_process(aur_helper, ["--noconfirm", "-Sua"])
+        spawn.process(aur_helper, ["--noconfirm", "-Sua"])
 
 def install(args=[]):
     """
     Install packages, no sync
     """
     if len(args) > 0:
-        spawn.sudo_process(aur_helper, ["--noconfirm", "-S"] + args)
+        spawn.process(aur_helper, ["--noconfirm", "-S"] + args)
