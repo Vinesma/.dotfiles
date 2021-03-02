c35b45e27ec0c250d2608cb1f5f8cacd3115e3b6
Vinesma <otaviocos_13@hotmail.com>
2021-03-02 17:39:08 -0300

    Remove descriptions of keybinds
    This was done because none of my keybinds were working anymore after adding descriptions

--- qtile/modules/keybinds.py
+++ qtile/modules/keybinds.py
@@ -49,113 +49,113 @@ def create_keybinds():
     """
     keys = [
         # Switch between windows in current stack pane
-        Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-        Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-        Key([mod], "Left", lazy.layout.left(), desc="Move focus left"),
-        Key([mod], "Right", lazy.layout.right(), desc="Move focus right"),
-        Key(["mod1"], "Tab", lazy.layout.next(), desc="Move window focus to next window"),
+        Key([mod], "Down", lazy.layout.down()),
+        Key([mod], "Up", lazy.layout.up()),
+        Key([mod], "Left", lazy.layout.left()),
+        Key([mod], "Right", lazy.layout.right()),
+        Key(["mod1"], "Tab", lazy.layout.next()),
 
         # Move windows up or down in current stack
-        Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
-        Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
-        Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
-        Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+        Key([mod, "shift"], "Down", lazy.layout.shuffle_down()),
+        Key([mod, "shift"], "Up", lazy.layout.shuffle_up()),
+        Key([mod, "shift"], "Left", lazy.layout.shuffle_left()),
+        Key([mod, "shift"], "Right", lazy.layout.shuffle_right()),
 
         # Grow windows in current stack
-        Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window downwards"),
-        Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window upwards"),
-        Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
-        Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+        Key([mod, "control"], "Down", lazy.layout.grow_down()),
+        Key([mod, "control"], "Up", lazy.layout.grow_up()),
+        Key([mod, "control"], "Left", lazy.layout.grow_left()),
+        Key([mod, "control"], "Right", lazy.layout.grow_right()),
 
         # Swap panes of split stack
-        Key([mod, "shift"], "space", lazy.layout.rotate(), desc="Swap windows in a split stack"),
+        Key([mod, "shift"], "space", lazy.layout.rotate()),
 
         # Toggle between split and unsplit sides of stack.
         # Split = all windows displayed
         # Unsplit = 1 window displayed, like Max layout, but still with
         # multiple stack panes
         Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
-        Key([mod], "Return", lazy.spawn(terminal), desc="Spawn terminal"),
+        Key([mod], "Return", lazy.spawn(terminal)),
 
         # Toggle between different layouts as defined below
-        Key([mod], "Tab", lazy.next_layout(), desc="Switch to next layout"),
-        Key([mod], "w", lazy.window.kill(), desc="Kill focused window's process"),
+        Key([mod], "Tab", lazy.next_layout()),
+        Key([mod], "w", lazy.window.kill()),
 
-        Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-        Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+        Key([mod, "control"], "r", lazy.restart()),
+        Key([mod, "control"], "q", lazy.shutdown()),
 
         # Toggle between floating window on/off
         Key([mod], "t", lazy.window.toggle_floating()),
 
         # Custom commands
         # emulate win ctrl+alt+del (spawn task manager)
-        Key(["mod1", "control"], "Delete", lazy.spawn(f"{terminal_reduced_opacity} htop"), desc="Task manager wannabe"),
+        Key(["mod1", "control"], "Delete", lazy.spawn(f"{terminal_reduced_opacity} htop")),
         # rofi
-        Key([mod], "r", lazy.spawn("rofi -show run"), desc="Spawn rofi in run mode"),
-        Key([mod], "e", lazy.spawn("rofi -show window"), desc="Spawn rofi in window mode"),
+        Key([mod], "r", lazy.spawn("rofi -show run")),
+        Key([mod], "e", lazy.spawn("rofi -show window")),
         # spawn apps
-        Key([mod, "mod1"], "e", lazy.spawn(file_browser), desc="Spawn file browser"),
-        Key([mod, "mod1"], "b", lazy.spawn(browser), desc="Spawn web browser"),
-        Key([mod, "mod1"], "n", lazy.spawn(f"{terminal_reduced_opacity} -T \"newsboat\" {scripts_path}/newsboat/open-newsboat.sh"), desc="Spawn rss reader"),
-        Key([mod, "mod1"], "m", lazy.spawn(f"{terminal_increased_opacity} -T \"MPD\" ncmpcpp -q"), desc="Spawn music player"),
-        Key([mod, "shift"], "m", lazy.spawn(path.join(scripts_path, "ncmpcpp-cover", "kitty-ncmpcpp.sh"), desc="Spawn music player + album covers")),
-        Key([mod, "mod1"], "l", lazy.spawn(f"{terminal_reduced_opacity} neomutt"), desc="Spawn email client"),
-        Key([mod, "mod1"], "i", lazy.spawn(path.join(scripts_path, "wifi-finder.sh")), desc="Spawn wifi finder script"),
-        Key([mod, "control"], "m", lazy.spawn(path.join(scripts_path, "song-browser.sh")), desc="Spawn song browser script"),
+        Key([mod, "mod1"], "e", lazy.spawn(file_browser)),
+        Key([mod, "mod1"], "b", lazy.spawn(browser)),
+        Key([mod, "mod1"], "n", lazy.spawn(f"{terminal_reduced_opacity} -T \"newsboat\" {scripts_path}/newsboat/open-newsboat.sh")),
+        Key([mod, "mod1"], "m", lazy.spawn(f"{terminal_increased_opacity} -T \"MPD\" ncmpcpp -q")),
+        Key([mod, "shift"], "m", lazy.spawn(path.join(scripts_path, "ncmpcpp-cover", "kitty-ncmpcpp.sh"))),
+        Key([mod, "mod1"], "l", lazy.spawn(f"{terminal_reduced_opacity} neomutt")),
+        Key([mod, "mod1"], "i", lazy.spawn(path.join(scripts_path, "wifi-finder.sh"))),
+        Key([mod, "control"], "m", lazy.spawn(path.join(scripts_path, "song-browser.sh"))),
         # take screenshot, either fullscreen or a selection
-        Key([mod], "Print", lazy.spawn(path.join(scripts_path, "take-screenshot.sh")), desc="Take a fullscreen screenshot"),
-        Key([mod, "mod1"], "Print", lazy.spawn(path.join(scripts_path, "take-screenshot.sh select")), desc="Take a screenshot of a selection"),
+        Key([mod], "Print", lazy.spawn(path.join(scripts_path, "take-screenshot.sh"))),
+        Key([mod, "mod1"], "Print", lazy.spawn(path.join(scripts_path, "take-screenshot.sh select"))),
         # wallpaper setter
-        Key([mod, "mod1"], "w", lazy.spawn(path.join(scripts_path, "bg-setter", "choose-bg.sh")), desc="Spawn wallpaper setter script"),
+        Key([mod, "mod1"], "w", lazy.spawn(path.join(scripts_path, "bg-setter", "choose-bg.sh"))),
         # mpc (mpd controller)
-        Key([], "Pause", lazy.spawn("mpc toggle"), desc="Toggle music play/pause"),
-        Key([mod, "mod1"], "Pause", lazy.spawn("mpc seek 0%"), desc="Restart music"),
+        Key([], "Pause", lazy.spawn("mpc toggle")),
+        Key([mod, "mod1"], "Pause", lazy.spawn("mpc seek 0%")),
         Key([mod], "Page_Up",
-            lazy.spawn(path.join(scripts_path, "query-vol.sh +10"), desc="Increase music volume"),
+            lazy.spawn(path.join(scripts_path, "query-vol.sh +10")),
         ),
         Key([mod], "Page_Down",
-            lazy.spawn(path.join(scripts_path, "query-vol.sh -10"), desc="Decrease music volume"),
+            lazy.spawn(path.join(scripts_path, "query-vol.sh -10")),
         ),
         Key([mod, "mod1"], "Page_Up",
-            lazy.spawn(path.join(scripts_path, "query-vol.sh +20"), desc="Greatly increase music volume"),
+            lazy.spawn(path.join(scripts_path, "query-vol.sh +20")),
         ),
         Key([mod, "mod1"], "Page_Down",
-            lazy.spawn(path.join(scripts_path, "query-vol.sh -20"), desc="Greatly decrease music volume"),
+            lazy.spawn(path.join(scripts_path, "query-vol.sh -20")),
         ),
-        Key([mod], "Home", lazy.spawn("mpc seek +10"), desc="Seek forwards in music"),
-        Key([mod], "End", lazy.spawn("mpc seek -10"), desc="Seek backwards in music"),
-        Key([mod], "Insert", lazy.spawn("mpc stop"), desc="Stop music"),
-        Key([mod], "period", lazy.spawn("mpc next"), desc="Skip to next track"),
-        Key([mod], "comma", lazy.spawn("mpc prev"), desc="Skip to previous track"),
+        Key([mod], "Home", lazy.spawn("mpc seek +10")),
+        Key([mod], "End", lazy.spawn("mpc seek -10")),
+        Key([mod], "Insert", lazy.spawn("mpc stop")),
+        Key([mod], "period", lazy.spawn("mpc next")),
+        Key([mod], "comma", lazy.spawn("mpc prev")),
         Key([mod], "F9",
-            lazy.spawn(path.join(scripts_path, "queue-clear.sh"), desc="Clear music queue"),
+            lazy.spawn(path.join(scripts_path, "queue-clear.sh")),
         ),
         Key([mod], "F10",
-            lazy.spawn(path.join(scripts_path, "queue-songs.sh Calm"), desc="Queue all calm tracks"),
+            lazy.spawn(path.join(scripts_path, "queue-songs.sh Calm")),
         ),
         Key([mod], "F11",
-            lazy.spawn(path.join(scripts_path, "queue-songs.sh Fast"), desc="Queue all fast tracks"),
+            lazy.spawn(path.join(scripts_path, "queue-songs.sh Fast")),
         ),
         Key([mod], "F12",
-            lazy.spawn(path.join(scripts_path, "queue-songs.sh Epic"), desc="Queue all epic tracks"),
+            lazy.spawn(path.join(scripts_path, "queue-songs.sh Epic")),
         ),
         # mpv (video player)
-        Key([mod, "mod1"], "p", lazy.spawn(path.join(scripts_path, "clipboard-mpv.sh")), desc="Spawn mpv controller script for watching and resuming videos"),
+        Key([mod, "mod1"], "p", lazy.spawn(path.join(scripts_path, "clipboard-mpv.sh"))),
         # youtube-dl mass downloading
-        Key([mod, "mod1"], "y", lazy.spawn(path.join(scripts_path, "youtube-dl-queuer", "youtube-dl-queuer.sh")), desc="Spawn youtube-dl helper script for downloading a queue of videos"),
-        Key([mod, "control"], "y", lazy.spawn(path.join(scripts_path, "youtube-dl-queuer", "hot-queue.sh")), desc="Quickly queue a video from the clipboard"),
+        Key([mod, "mod1"], "y", lazy.spawn(path.join(scripts_path, "youtube-dl-queuer", "youtube-dl-queuer.sh"))),
+        Key([mod, "control"], "y", lazy.spawn(path.join(scripts_path, "youtube-dl-queuer", "hot-queue.sh"))),
     ]
 
     # Create keybinds for switching groups
     for group in groups.create_groups():
         keys.extend([
             # mod1 + letter of group = switch to group
-            Key([mod], group.name, lazy.group[group.name].toscreen(), desc="Switch to this group"),
+            Key([mod], group.name, lazy.group[group.name].toscreen()),
 
             # mod1 + shift + letter of group = send to group
-            Key([mod, "shift"], group.name, lazy.window.togroup(group.name, switch_group=False), desc="Send window to this group"),
+            Key([mod, "shift"], group.name, lazy.window.togroup(group.name, switch_group=False)),
             # mod1 + control + letter of group = send to group & switch screens
-            Key([mod, "control"], group.name, lazy.window.togroup(group.name, switch_group=True), desc="Send window to this group and also switch to it"),
+            Key([mod, "control"], group.name, lazy.window.togroup(group.name, switch_group=True)),
     ])
 
     return keys
