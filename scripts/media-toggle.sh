b7f50477c0012491d93598ef53b97fd583820c9a
Otavio Cornelio <otaviocos_13@hotmail.com>
2021-09-05 23:48:46 -0300

    Handle ambiguity in media-toggle script

--- scripts/media-toggle.sh
+++ scripts/media-toggle.sh
@@ -5,8 +5,8 @@
 
 mpv_socket="/tmp/mpvsocket"
 
-mpc toggle
-
-if [[ -e "$mpv_socket" ]]; then
-    echo 'cycle pause' | socat - "$mpv_socket"
+# try toggling mpv, if it doesn't work, toggle mpc instead
+# this gives mpv priority when command could apply to both
+if ! echo 'cycle pause' | socat - "$mpv_socket"; then
+    mpc toggle
 fi
