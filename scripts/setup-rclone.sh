c81f1439daf65bc6f22ba10a975c572308c17066
Otavio Cornelio <otaviocos_13@hotmail.com>
2021-12-05 17:46:44 -0300

    rclone: add fallback config directory if first is not found

--- scripts/setup-rclone.sh
+++ scripts/setup-rclone.sh
@@ -5,6 +5,7 @@
 #
 
 config_source="$HOME/Sync/rclone/rclone.conf"
+fallback_config="/run/media/$USER/OTDRIVE/rclone/rclone.conf"
 config_dir="$HOME/.config/rclone/"
 
 printf "%s\n" "Installing rclone"
@@ -13,6 +14,9 @@ curl https://rclone.org/install.sh | sudo bash
 if [ -e "$config_source" ]; then
     mkdir -p "$config_dir"
     cp -v "$config_source" "$config_dir/rclone.conf"
+elif [ -e "$fallback_config" ]; then
+    mkdir -p "$config_dir"
+    cp -v "$fallback_config" "$config_dir/rclone.conf"
 else
     printf "%s" "Configuration file not found! Create one using ´rclone config´ or place it manually in '$config_dir'."
 fi
