6b5d0d38160e97b086a51c06a306b6b47c020e46
Otavio Cornelio <otaviocos_13@hotmail.com>
2021-12-26 12:40:08 -0300

    lightdm: automatically config DM on install

---
+++ install/scripts/setup-display-manager.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Setup default DM configuration and create a dedicated wallpaper folder for DMs to use.
+
+DM_NAME=lightdm
+DM_CONFIG_DIR=/etc/$DM_NAME
+DM_WALLPAPER_DIR=/usr/share/wallpapers
+WORK_DIR=$HOME/.dotfiles/$DM_NAME
+
+# -- SETUP --
+if [ ! -d $DM_WALLPAPER_DIR ]; then
+    sudo mkdir -pv $DM_WALLPAPER_DIR
+fi
+
+sudo chmod -v 777 $DM_WALLPAPER_DIR
+
+# -- ACT --
+for file in "$WORK_DIR"/*; do
+    sudo cp -vf "$file" "$DM_CONFIG_DIR"
+done
--- install/scripts/setup-numlockx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Setup numlockx
-
-# -- ACT --
-sudo sed -i -Ee 's/#?greeter-setup-script=.*/greeter-setup-script=\/usr\/bin\/numlockx on/' \
-    /etc/lightdm/lightdm.conf
