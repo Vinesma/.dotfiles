3aa3c120e794a7dd78ab18ba87970421729513e1
Otavio Cornelio <otaviocos_13@hotmail.com>
2021-12-31 15:54:34 -0300

    newsboat: remove db sync, use flock

--- scripts/newsboat/newsboat-reloader.sh
+++ scripts/newsboat/newsboat-reloader.sh
@@ -2,38 +2,18 @@
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1000/bus'
 
-newsboat_path="/usr/bin/newsboat"
-notify_send_path="/usr/bin/notify-send"
-cut_path="/usr/bin/cut"
-stat_path="/usr/bin/stat"
-icon="/usr/share/icons/Papirus/32x32/apps/akregator.svg"
-icon_sync="/usr/share/icons/Papirus/32x32/apps/syncthing-gtk.svg"
-folder="$HOME/.dotfiles/scripts/newsboat"
-sync_folder="$HOME/Sync"
+NEWSBOAT="/usr/bin/newsboat"
+NEWSBOAT_SCRIPTS_DIR="$HOME/.dotfiles/scripts/newsboat"
+LOCKFILE=/tmp/newsboat.lock
+CUT="/usr/bin/cut"
+FLOCK="/usr/bin/flock"
+NOTIFY_SEND="/usr/bin/notify-send"
+ICON_RSS="/usr/share/icons/Papirus/32x32/apps/akregator.svg"
 
-# This check prevents newsboat from running on top of another instance and overwriting files
-if [[ ! -e "/tmp/run-newsboat.tmp" ]]; then
-    /usr/bin/touch "/tmp/run-newsboat.tmp"
+$FLOCK -n $LOCKFILE $NEWSBOAT -x reload || exit 1
 
-    "$newsboat_path" -x reload
+if unread_count=$($FLOCK -n $LOCKFILE $NEWSBOAT -x print-unread); then
+    "$NOTIFY_SEND" -i "$ICON_RSS" "NEWSBOAT" "$unread_count"
 
-    last_read_saved=$(< "$folder/archive_last_read.tmp")
-    last_read_time=$("$stat_path" -c %Y "$sync_folder/.newsboat_archive")
-
-    if [[ -e "$sync_folder/.newsboat_archive" ]] && [[ "$last_read_saved" != "$last_read_time" ]]; then
-        "$newsboat_path" -q -I "$sync_folder/.newsboat_archive"
-        "$notify_send_path" -i "$icon_sync" "Syncthing" "[Newsboat] Sync successful!"
-    fi
-
-    if [[ -e "$sync_folder" ]]; then
-        "$newsboat_path" -q -E "$sync_folder/.newsboat_archive"
-        "$stat_path" -c %Y "$sync_folder/.newsboat_archive" > "$folder/archive_last_read.tmp"
-    fi
-
-    if unread=$("$newsboat_path" -x print-unread); then
-        "$notify_send_path" -i "$icon" "NEWSBOAT" "$unread"
-        echo "$unread" | "$cut_path" -d ' ' -f 1 > "$folder/unread_count.tmp"
-    fi
-
-    /usr/bin/rm "/tmp/run-newsboat.tmp"
+    printf "%s" "$unread_count" | "$CUT" -d ' ' -f 1 > "$NEWSBOAT_SCRIPTS_DIR/unread_count.tmp"
 fi
--- scripts/newsboat/open-newsboat.sh
+++ scripts/newsboat/open-newsboat.sh
@@ -4,30 +4,14 @@
 # Dependencies
 # - newsboat
 
-folder="$HOME/.dotfiles/scripts/newsboat"
-sync_folder="$HOME/Sync"
-last_read_saved=$(< "$folder/archive_last_read.tmp")
-last_read_time=$(stat -c %Y "$sync_folder/.newsboat_archive")
-icon_error="/usr/share/icons/Papirus/32x32/status/dialog-error.svg"
+NEWSBOAT_SCRIPTS_DIR="$HOME/.dotfiles/scripts/newsboat"
+LOCKFILE=/tmp/newsboat.lock
+ICON_ERROR="/usr/share/icons/Papirus/32x32/status/dialog-error.svg"
 
-# This check prevents newsboat from running on top of another instance and overwriting files
-if [[ ! -e "/tmp/run-newsboat.tmp" ]]; then
-    touch "/tmp/run-newsboat.tmp"
-
-    if [[ -e "$sync_folder/.newsboat_archive" ]] && [[ "$last_read_saved" != "$last_read_time" ]]; then
-        newsboat -q -I "$sync_folder/.newsboat_archive"
+if flock -n $LOCKFILE newsboat -q; then
+    if unread_count=$(flock -n $LOCKFILE newsboat -x print-unread); then
+        printf "%s" "$unread_count" | cut -d ' ' -f 1 > "$NEWSBOAT_SCRIPTS_DIR/unread_count.tmp"
     fi
-
-    newsboat -q
-
-    newsboat -x print-unread | cut -d' ' -f 1 > "$folder/unread_count.tmp"
-
-    if [[ -e "$sync_folder" ]]; then
-        newsboat -q -E "$sync_folder/.newsboat_archive"
-        stat -c %Y "$sync_folder/.newsboat_archive" > "$folder/archive_last_read.tmp"
-    fi
-
-    rm "/tmp/run-newsboat.tmp"
 else
-    notify-send -i "$icon_error" "NEWSBOAT" "Newsboat is already running, either wait for it to complete or exit the other instance."
+    notify-send -i "$ICON_ERROR" "NEWSBOAT" "Newsboat is already running, either wait for it to complete or exit the other instance."
 fi
