95a3a614de49baaa70d9e4d3412c44985c552f06
Otavio Cornelio <otaviocos_13@hotmail.com>
2022-01-15 10:18:59 -0300

    install-script: add wallpaper hook to autorandr

--- install/scripts/setup-multihead.sh
+++ install/scripts/setup-multihead.sh
@@ -7,12 +7,24 @@ POSTSWITCH_EXTRA_DIR=$WORK_DIR/postswitch.d
 PREDETECT_SCRIPT=$WORK_DIR/predetect
 POLYBAR_SCRIPT=$HOME/.dotfiles/polybar/launch.sh
 
+# -- SETUP --
+mkdir -p "$WORK_DIR"
+
 # -- ACT --
 
 # predetect hook
 printf "%s\n" \
     '#!/usr/bin/env bash' \
     "sleep 1" > "$PREDETECT_SCRIPT"
+
+# wallpaper setter hook
+# shellcheck disable=SC2016
+if command -v feh &> /dev/null; then
+    printf "%s\n" \
+        'feh --bg-scale "$(< "${HOME}/.cache/wal/wal")" &' \
+        > "$POSTSWITCH_EXTRA_DIR/set-wallpaper" \
+        && chmod 777 "$POSTSWITCH_EXTRA_DIR/set-wallpaper"
+fi
 
 # polybar hook
 if command -v polybar &> /dev/null; then
