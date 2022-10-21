7efe8963ad0cd983365df26f0c4db013b6b30f64
Otavio Cornelio <otaviocos_13@hotmail.com>
2022-10-20 21:35:14 -0300

    gtk: create gtk file and add it to installer

--- install/scripts/setup-pywal.sh
+++ install/scripts/setup-pywal.sh
@@ -19,9 +19,16 @@ ln -sfv "$MY_TEMPLATES_DIR/colors-polybar.ini" "$WAL_TEMPLATES_DIR"
 # Create links for zathura colors
 ln -sfv "$MY_TEMPLATES_DIR/colors-zathura" "$WAL_TEMPLATES_DIR"
 
+# Create links for gtk colors
+ln -sfv "$MY_TEMPLATES_DIR/gtk.css" "$WAL_TEMPLATES_DIR"
+
 # Create default color scheme
 wal -e -t -s -n -i "$DEFAULT_WALLPAPER"
 
 # Link generated dunst config file to the proper place
 mkdir -p "$HOME/.config/dunst"
 ln -sfv "$HOME/.cache/wal/colors-dunst" "$HOME/.config/dunst/dunstrc"
+
+# Link generated gtk colors file
+mkdir -p "$HOME/.config/gtk-3.0"
+ln -sfv "$HOME/.cache/wal/gtk.css" "$HOME/.config/gtk-3.0"
