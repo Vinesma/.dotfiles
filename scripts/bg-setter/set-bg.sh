4641f94e580024f49c73bbdfd5f462a3c661464a
Otavio Cornelio <otaviocos_13@hotmail.com>
2023-02-17 12:45:07 -0300

    pywal: minify Jellyfin CSS, update it in JF when colors are changed

--- scripts/bg-setter/set-bg.sh
+++ scripts/bg-setter/set-bg.sh
@@ -6,6 +6,7 @@
 
 icon_error="/usr/share/icons/Papirus/32x32/status/dialog-error.svg"
 icon_image="/usr/share/icons/Papirus/32x32/apps/multimedia-photo-viewer.svg"
+jellyfin_css_path="$HOME/.cache/wal/colors-jellyfin.css"
 notify_time=2000
 
 send-error() {
@@ -15,6 +16,21 @@ send-error() {
 # Quick and dirty patch to the rofi result template since it looks like upstream will take thousands of years to update.
 patch-rofi() {
     sed -i -e '$a\\nelement-text {\n    background-color: inherit;\n    text-color: inherit;\n}' ~/.cache/wal/colors-rofi-dark.rasi
+}
+
+# POST request to update server side CSS
+# Uses env variables, if unset does nothing
+update-jellyfin() {
+    if [[ -n "$JF_SERVER_URL" && -n "$JF_API_KEY" ]]; then
+        local rawCSS
+        rawCSS=$(< "$jellyfin_css_path")
+        curl \
+        "$JF_SERVER_URL/System/Configuration/branding?api_key=$JF_API_KEY" \
+        -X POST \
+        -H 'Content-Type: application/json' \
+        --data-raw "{\"LoginDisclaimer\":\"\",\"CustomCss\":\"${rawCSS}\",\"SplashscreenEnabled\":false}" && \
+        notify-send -i "$icon_image" "set-bg" "Jellyfin branding updated."
+    fi
 }
 
 # Reset polybar if in use, otherwise do nothing
@@ -37,6 +53,7 @@ if wal -n -e -i "$@"; then
     reset-polybar
 
     notify-send -i "$icon_image" "set-bg" "New background and theme set."
+    update-jellyfin
 else
     send-error "pywal returned an error"
 fi
