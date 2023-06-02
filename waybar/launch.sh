22ff87f1a0c831ee7b4834ccc8c1ed6728250bac
Otavio Cornelio <otaviocos_13@hotmail.com>
2023-06-02 19:21:15 -0300

    waybar: add config + launch script

---
+++ waybar/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Main config path
+CONFIG_FILE=$HOME/.dotfiles/waybar/config
+STYLE_FILE=$HOME/.cache/wal/waybar-style.css
+temp_file=/tmp/waybar.tmp
+
+# Terminate already running bar instances
+killall -q waybar
+while pgrep waybar &> /dev/null; do
+    sleep 0.3
+done
+
+printf "%s\n" "--- LAUNCH waybar ---" | tee -a "$temp_file"
+waybar --config "$CONFIG_FILE" --style "$STYLE_FILE" 2>&1 | tee -a "$temp_file" & disown
+
+printf "%s\n" "[Waybar]: Bar launched..."
