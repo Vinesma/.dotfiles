25e3b96429bd6c20858842b5450e09f787f35472
Otavio Cornelio <otaviocos_13@hotmail.com>
2023-06-05 20:26:14 -0300

    scripts: update clipboard-mpv to use rofi or wofi

--- scripts/mpv/clipboard-mpv.sh
+++ scripts/mpv/clipboard-mpv.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env bash
 #
-# Script meant to be run by a keybind, opens a rofi menu for watching videos in mpv.
+# Script meant to be run by a keybind, opens a rofi/wofi menu for watching videos in mpv.
 # Features:
 # - VIDEO: Choose video format and watch straight from the rofi menu after a single keypress
 # Dependencies:
-# mpv, xclip, yt-dlp, rofi
+# mpv, xclip/cliphist, yt-dlp, rofi/wofi
 
 notify_time=2000
 
 icon_error="/usr/share/icons/Papirus/32x32/status/dialog-error.svg"
 icon_youtube_dl="/usr/share/icons/Papirus/32x32/apps/youtube-dl.svg"
 icon_mpv="/usr/share/icons/Papirus/32x32/apps/mpv.svg"
+session_wrofi="$HOME/.dotfiles/scripts/helpers/session-wrofi.sh"
 
 folder_watchlater="$HOME/Sync/.mpv_files"
 folder_videos="$HOME/Videos"
-default_formats="webm[height=480]+bestaudio/webm[height=720]+bestaudio/22/18/480p/360p/720p"
+default_formats="webm[height=720]+bestaudio/webm[height=480]+bestaudio/22/18/720p/480p/360p"
 
-# Check if script is passed and argument or not.
+# Check if script is passed an argument or not.
 # If yes, use the argument
 # If no, use the clipboard.
-[ "$#" -gt 0 ] && link="$1" || link="$(xclip -selection clipboard -o)"
+if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
+    clipboard_text=$(xclip -selection clipboard -o)
+else
+    clipboard_text=$(cliphist list | head -n 1 | cliphist decode)
+fi
+
+link=${1:-$clipboard_text}
+# shellcheck source=../helpers/session-wrofi.sh
+source "$session_wrofi"
 
 send-error() {
     notify-send -i "$icon_error"  -t "$notify_time" "MPV" "$1"
@@ -27,26 +36,52 @@ send-error() {
 
 start-playback() {
     local timestamp
+    local wrofi_args
+    local mpv_args
 
-    timestamp="$(echo -e '0%\n25%\n50%\n75%\n90%' | rofi -dmenu -p 'Timestamp' -lines 5 2> /dev/null)"
+    if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
+        wrofi_args=(\
+            "-p" "Timestamp"
+            "-theme-str" "listview {lines: 5;}")
+    else
+        wrofi_args=(\
+            "-p" "Timestamp"
+            "--lines" "6"
+        )
+    fi
+
+    timestamp="$(echo -e '0%\n25%\n50%\n75%\n90%' | wrofi-switch "${wrofi_args[@]}" 2> /dev/null)"
+    mpv_args=("--no-terminal" "--ytdl-format=$1" "$link")
 
     notify-send -i "$icon_mpv" -t "$notify_time" "MPV" "Starting playback..."
-    if [[ "$timestamp" = "0%" ]]; then
-        if ! mpv --no-terminal --ytdl-format="$1" "$link"; then
-            send-error "Error during playback"
-        fi
-    else
-        if ! mpv --no-terminal --ytdl-format="$1" --start="$timestamp" "$link"; then
-            send-error "Error during playback"
-        fi
+
+    if [[ "$timestamp" != "0%" ]]; then
+        mpv_args=("${mpv_args[@]}" "--start=$timestamp")
+    fi
+
+    if ! mpv "${mpv_args[@]}"; then
+        send-error "Error during playback"
     fi
 }
 
 show-format-menu() {
     local format
+    local wrofi_args
+
+    if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
+        wrofi_args=(\
+            "-p" "Video Format"
+            "-theme-str" "listview {lines: 15;}")
+    else
+        wrofi_args=(\
+            "-p" "Video Format"
+            "--lines" "16"
+        )
+    fi
+
 
     format=$(echo -e "$1\nExit" | \
-    rofi -dmenu -only-match -p 'Video format' -lines 15 2> /dev/null | \
+    wrofi-switch "${wrofi_args[@]}" 2> /dev/null | \
     cut -d' ' -f 1)
 
     [[ "$format" != "Exit" ]] && start-playback "$format" || \
@@ -79,9 +114,20 @@ get-video-info() {
 
 show-resume-menu() {
     local choice
+    local wrofi_args
 
-    choice=$(echo -e "$1\n Exit" | \
-        rofi -dmenu -only-match -p 'Choice' -lines 10 2> /dev/null)
+    if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
+        wrofi_args=(\
+            "-p" "Choice"
+            "-theme-str" "listview {lines: 10;}")
+    else
+        wrofi_args=(\
+            "-p" "Choice"
+            "--lines" "11"
+        )
+    fi
+
+    choice=$(echo -e "$1\n Exit" | wrofi-switch "${wrofi_args[@]}" 2> /dev/null)
 
     if [[ "$choice" != " Exit" ]]; then
         if [[ "$choice" != @(*youtube.com/watch*|*youtu.be*|*twitch.tv/videos*) ]]; then
@@ -102,6 +148,18 @@ show-menu() {
     local option
     local resume
     local lines
+    local wrofi_args
+
+    if [ "$XDG_SESSION_TYPE" != "wayland" ]; then
+        wrofi_args=(\
+            "-p" "Option"
+            "-theme-str" "listview {lines: 10;}")
+    else
+        wrofi_args=(\
+            "-p" "Option"
+            "--lines" "10"
+        )
+    fi
 
     resume=$(grep -hve "# redirect entry" "$folder_watchlater"/* 2>/dev/null | grep "#")
 
@@ -111,10 +169,10 @@ show-menu() {
 
         if [[ -n "$resume" ]]; then
             lines="$(( lines + 2 ))"
-            option=$(echo -e "$option菱 Resume watching\n Exit" | rofi -dmenu -only-match -p 'Option' -lines "$lines" 2> /dev/null)
+            option=$(echo -e "$option菱 Resume watching\n Exit" | wrofi-switch "${wrofi_args[@]}" 2> /dev/null)
         else
             lines="$(( lines + 1 ))"
-            option=$(echo -e "$option Exit" | rofi -dmenu -only-match -p 'Option' -lines "$lines" 2> /dev/null)
+            option=$(echo -e "$option Exit" | wrofi-switch "${wrofi_args[@]}" 2> /dev/null)
         fi
     else
         option=""
@@ -122,7 +180,7 @@ show-menu() {
 
         if [[ -n "$resume" ]]; then
             lines="$(( lines + 2 ))"
-            option=$(echo -e "$option菱 Resume watching\n Exit" | rofi -dmenu -only-match -p 'Option' -lines "$lines" 2> /dev/null)
+            option=$(echo -e "$option菱 Resume watching\n Exit" | wrofi-switch "${wrofi_args[@]}" 2> /dev/null)
         else
             send-error "Nothing to play or resume."
         fi
