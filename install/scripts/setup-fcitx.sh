acd34706254330f63b9e3bb9da5c5bc41679badf
Otavio Cornelio <otaviocos_13@hotmail.com>
2024-01-31 14:59:05 -0300

    env: remove fcitx env var that is no longer needed

--- install/scripts/setup-fcitx.sh
+++ install/scripts/setup-fcitx.sh
@@ -4,7 +4,6 @@
 
 # -- ACT --
 printf '\n%s' \
-    'GTK_IM_MODULE=fcitx' \
     'QT_IM_MODULE=fcitx' \
     'XMODIFIERS=@im=fcitx' \
     'GLFW_IM_MODULE=ibus' | sudo tee -a /etc/environment > /dev/null
