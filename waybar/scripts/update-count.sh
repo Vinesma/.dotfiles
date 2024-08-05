210a22d3d1116082792697434964c6439d628751
Otavio Cornelio <otaviocos_13@hotmail.com>
2024-08-04 21:00:01 -0300

    waybar: improvements and fixes

--- waybar/scripts/update-count.sh
+++ waybar/scripts/update-count.sh
@@ -5,7 +5,7 @@ if ! update_count=$(checkupdates 2> /dev/null | wc -l); then
     update_count=0
 fi
 
-if [ "$update_count" -gt 0 ]; then
+if [ "$update_count" -gt 80 ]; then
     printf "%s" "$update_count"
 else
     printf "%s" ""
